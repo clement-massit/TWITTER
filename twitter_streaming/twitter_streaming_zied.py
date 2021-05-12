fa93d255c1e6a7aa4c4d825fc6530e94d4bb8000
ZiedGOBJI <zied.gobji@protonmail.ch>
2021-05-12 20:00:54 +0200

    création classes TwitterClient et TwitterAuthenticator et possibilité de récupérer la timeline d'un user

--- twitter_streaming/twitter_streaming_zied.py
+++ twitter_streaming/twitter_streaming_zied.py
@@ -1,3 +1,6 @@
+from tweepy import API 
+from tweepy import Cursor 
+
 # StreamListener est une classe de Tweepy qui nous permet d'écouter les tweets
 from tweepy.streaming import StreamListener
 
@@ -10,15 +13,114 @@ from tweepy import Stream
 from twitter_streaming import credentials 
 
 
+
+
+
+# Création d'une classe réservée à l'interaction avec le client Tiwtter
+
+class TwitterClient():
+
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = [] 
+        # Obtenir les tweets de la timeline du user (celui qui run le program par défaut)
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+
+
+
+
+
+# Création d'une classe réservée à l'authentification
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+
+        auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+        auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_SECRET)
+
+        return auth
+
+
+# On crée une classe de streamer 
+class TwitterStreamer():
+    '''
+    Permet le streaming et le traitement de tweets en live
+    '''
+
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+
+    # Prend en paramètre un filename d'un .txt qui contiendra nos tweets streamés 
+    def stream_tweets(self, feteched_tweets_filename, hash_tag_list):
+        # Gère l'authentification et la connexion à l'API de streaming 
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
+        
+        # On instancie un objet de la classe Stream, qui va nous permettre de récupérer les tweets
+        stream = Stream(auth, listener)
+
+        # On se doit de trier les tweets que l'on souhaite récupérer, on utiliser ainsi la méthode filter de la classe Stream
+        # La liste track est une liste de keywords 
+        stream.filter(track = hash_tag_list)
+
+
 # Création d'une classe qui hérite de StreamListener et va nous permettre d'afficher les tweets
-class StdOutListener(StreamListener):
 
+class TwitterListener(StreamListener):
+    '''
+    Un listener classique que affiche les tweets reçus
+    '''
+    
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+
+
+    # Gère la récupération des données
     def on_data(self, data):
-        pass
+        try:
+            print(data)
+            # On écrit les tweets dans un fichiers texte en continu
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
 
-    def on_error(self, status):
-        pass
+        # Si ça ne marche pas, on retourne l'erreur
+        except BaseException :
+            print("Erreur dans on_data: %s" %str(BaseException))
+        return True
+    
+    # méthode qui intervient lorsqu'il y a une erreur
+    def on_error(self, status): 
+        # L'erreur 420 correspond à la rate limit, comme ça si on a un pb on sait tout de suite s'il s'agit de la rate limit ou non         
+        if status == 420:
+            return False
+        # on affiche la variable status, qui affichera la nature de l'erreur
+        print(status)
 
+
+if __name__ == "__main__":
+
+    hash_tag_list = ['','','']
+    fetched_tweets_filename = "tweets.json"
+
+# On définit un twitter client
+    twitter_client = TwitterClient('BillGates')
+    print(twitter_client.get_user_timeline_tweets(5))
+
+
+# On définit un objet Streamer
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
 
 
 
