3ece4b3739fb9ede6c3b9eb475be7d66fbf432b6
clement-massit <clement.massit@gmail.com>
2021-05-15 19:21:44 +0200

    useless file

--- twitter_streaming/tweets_json_treatment.py
+++
@@ -1,8 +0,0 @@
-import json
-
-path = 'C:\\wamp64\\www\\TWITTER\\tweets json format\\tweets.json'
-with open(path, 'r') as tweet_file:
-    data = json.load(tweet_file)
-    print(data['created_at'])
-        
-
