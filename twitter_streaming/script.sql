71681721f67ea482f394aad6c22d12fe7fc82c09
clement-massit <clement.massit@gmail.com>
2021-05-15 02:07:29 +0200

    script SQL modified

--- twitter_streaming/script.sql
+++ twitter_streaming/script.sql
@@ -3,16 +3,16 @@ set NAMES 'UTF8';
 
 DROP TABLE IF EXISTS tweets_streaming;
 CREATE TABLE tweets_streaming (
-    id_tweet int(100) NOT NULL AUTO_INCREMENT, 
+    id_tweet int NOT NULL , 
     created_at varchar(100), 
     user_name varchar(100), 
     text_contenu varchar(255), 
     latitude varchar(100), 
     longitude varchar(100), 
     place varchar(100), 
-    id_place varchar(100),
-    PRIMARY KEY (id_tweet),
-    FOREIGN KEY (id_place) REFERENCES polygon(id_place)
+    id_place varchar(100)
+    
+   
 
 );
 
